0e6bb48ccc67a4b55e7d92757415c2e94401bdc9
Ozhey119 <ozhey119@gmail.com>
2020-11-23 23:49:38 +0200

    .

--- listings/views.py
+++ listings/views.py
@@ -1,16 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .models import Listing
 
 
-def index(request):
-    return render(request, 'listings/listings.html')
+def index(request): 
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = {'listings': paged_listings}
+
+    return render(request, 'listings/listings.html', context)
 
 
-def listing(request):
-    return render(request, 'listings/listing.html')
+def listing(request, listing_id):
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing': listing
+    }
+
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
     return render(request, 'listings/search.html')
-
-
-# Create your views here.
