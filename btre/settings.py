81d39f46f6f9204c6beb5ca3bc117922361a0afb
Ozhey119 <ozhey119@gmail.com>
2020-12-10 16:47:53 +0200

    added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -150,3 +150,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'israelrealestate2@gmail.com'
 EMAIL_HOST_PASSWORD = 'ireire123'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
